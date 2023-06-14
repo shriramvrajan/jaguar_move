16f524ff453a8ddfca0c56387f1607804b287bb0
shriramvrajan <shriramvrajan@gmail.com>
2023-06-13 20:51:08 -0400

    Prepare to work on 01_generate_data.R

--- R/00_functions.R
+++ R/00_functions.R
@@ -3,7 +3,7 @@ rm(list = ls())
 ## Basic functions and libraries
 
 # Libraries
-library(raster) # update to terra at some point
+library(terra)
 library(tidyverse)
 library(data.table)
 # library(ctmm)
@@ -342,17 +342,17 @@ plot_path <- function(path, vgram = FALSE, new = TRUE, ...) {
 # Data =========================================================================
 
 # WGS84 projection
-wgs84 <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+# wgs84 <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
 
-# Jaguar movement data, ID numbers, and metadata
-jag_move <- readRDS("data/jag_data_BR.RDS")
-jag_id <- readRDS("data/jag_list.RDS"); njag <- nrow(jag_id)
-jag_meta <- data.table(read.csv("data/input/jaguars/jaguar_metadata.csv"))
+# # Jaguar movement data, ID numbers, and metadata
+# jag_move <- readRDS("data/jag_data_BR.RDS")
+# jag_id <- readRDS("data/jag_list.RDS"); njag <- nrow(jag_id)
+# jag_meta <- data.table(read.csv("data/input/jaguars/jaguar_metadata.csv"))
 
-# RasterStack of environmental variables 
-# see 01_generate_data.R for details
-brazil_ras <- stack("data/env_layers.grd")
-# RasterStack of environmental variables, but as a data frame ('brdf')
-load("data/env_layers.RData")
+# # RasterStack of environmental variables 
+# # see 01_generate_data.R for details
+# brazil_ras <- stack("data/env_layers.grd")
+# # RasterStack of environmental variables, but as a data frame ('brdf')
+# load("data/env_layers.RData")
 
-msg("Loaded data")
+# msg("Loaded data")
